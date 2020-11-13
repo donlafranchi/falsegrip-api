2df60253f07d17fe0273a02168c506ced2d350a0
IT-corridor <it.corridor051@gmail.com>
2020-11-13 00:06:54 -0800

    add AppUserManager

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,21 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 from django.utils import timezone
 
 
+class AppUserManager(BaseUserManager):
+    def _create_user(self, username, password=None, is_superuser=False, **kwargs):
+        user = self.model(username=username, is_superuser=is_superuser, **kwargs)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, username, password=None, **kwargs):
+        return self._create_user(username, password, is_superuser=False, **kwargs)
+
+    def create_superuser(self, username, password, **kwargs):
+        return self._create_user(username, password, is_superuser=True, **kwargs)
+
+
 class AppUser(AbstractBaseUser, PermissionsMixin):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     username = models.CharField(max_length=50, unique=True, blank=False)
@@ -17,6 +32,8 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
     city = models.CharField(max_length=150, blank=True, null=True)
     is_active = models.BooleanField(default=True, verbose_name='Active')
     date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = AppUserManager()
 
     USERNAME_FIELD = 'username'
 
